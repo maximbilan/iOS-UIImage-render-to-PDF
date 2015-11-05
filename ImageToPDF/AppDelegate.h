0b04e1a7f1c4d94ba08d2639c7b22bfeb38bf1a2
Maxim Bilan <maximb.mail@gmail.com>
2015-11-05 23:44:19 +0200

    Added project template

---
+++ ImageToPDF/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageToPDF
+//
+//  Created by Maxim on 11/5/15.
+//  Copyright © 2015 Maxim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageToPDF/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageToPDF
+//
+//  Created by Maxim on 11/5/15.
+//  Copyright © 2015 Maxim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
